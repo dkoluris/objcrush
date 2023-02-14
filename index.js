72d9fc3a5a7e69543d2f6a7398b0e42960ae1fa9
Dennis Koluris <dkoluris@outlook.com>
2023-02-14 21:22:44 +0200

    Minor fix

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const objCrush = (obj = {}) => Object.keys(obj || {}).reduce((acc, cur) => {
     if (typeof obj[cur] === 'object') {
-        acc = { ...acc, ...crushObj(obj[cur])}
-    } else { acc[cur] = obj[cur] }
-    return acc
+        acc = { ...acc, ...objCrush(obj[cur]) }
+    } else {
+        acc[cur] = obj[cur];
+    }
+    return acc;
 }, {});
 
 module.exports = objCrush;
